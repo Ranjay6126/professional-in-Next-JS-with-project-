a31c1c5a8093385ed41d722c6aa2fece181f8aea
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-01 23:22:06 +0530

    next js learing

---
+++ components/Header.js
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header><h1>My Next.js App</h1></header>
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello, Next.js!</h1>
+      <p>Welcome to your first Next.js app.</p>
+    </div>
+  )
+}
